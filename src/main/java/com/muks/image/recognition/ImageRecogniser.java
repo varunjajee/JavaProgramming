c14e684b789aa6829c576c11529ee93a06fb0fba
Mukthar Ahamed <mukthar.ahmed@MUKS.local>
2015-06-10 14:32:49 +0530

    First commit

---
+++ src/main/java/com/muks/image/recognition/ImageRecogniser.java
@@ -0,0 +1,33 @@
+package com.muks.image.recognition;
+
+/*
+ * Created by mukthar.ahmed on 09/06/15.
+ */
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.io.File;
+
+public class ImageRecogniser {
+
+    /*
+        Commad to install tesseract on mac:
+        brew install tesseract --all-languages
+     */
+    public static void main(String[] args) {
+        //File imageFile = new File("/Users/mukthar.ahmed/Downloads/UseFull/longHair.jpg");
+        File imageFile = new File("/Users/mukthar.ahmed/Downloads/images.jpg");
+
+        Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
+        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+
+        try {
+            String result = instance.doOCR(imageFile);
+
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/muks/javascript/exec/engine/V8JavascriptEngine.java
@@ -0,0 +1,99 @@
+package com.muks.javascript.exec.engine;
+
+import javax.script.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+
+/**
+ * Hello world!
+ *
+ */
+public class V8JavascriptEngine {
+
+
+    //    public static void main(String[] args) {
+    //        V8 v8 = V8.createV8Runtime();
+    //        v8.executeScript("<script src=\\\"mraid.js\\\"></script><script language='Javascript1.1' "
+    //            + "SRC=\\\"https:\\/\\/pixel.adsafeprotected"
+    //            + ".com\\/rjss\\/st\\/39226\\/5024613\\/skeleton.js\\\"><\\/script>");
+    ////        v8.registerJavaMethod(new Printer(), "print", "print", new Class<?>[]{String.class});
+    ////        v8.executeVoidScript( "print('Hello, World!');" );
+    //        v8.release(true);
+
+    //
+    //        //ScriptEngine engine = new ScriptEngineManager(null).getEngineByName("nashorn");
+    //        ScriptEngineManager manager = new ScriptEngineManager();
+    //
+    //        try {
+    //      engine.eval("print('Hello World!');");
+    //        } catch (ScriptException e) {
+    //            e.printStackTrace();
+    //        }
+    //
+    //
+    //        try {
+    //            engine.eval(new FileReader("/Users/mukthar.ahmed/Downloads/script.js"));
+    //        } catch (ScriptException e) {
+    //            e.printStackTrace();
+    //        } catch (FileNotFoundException e) {
+    //            e.printStackTrace();
+    //        }
+    //
+
+
+    public static void main(String args[]) {
+        //String jsScript = "https://pixel.adsafeprotected.com/rjss/st/39226/5024613/skeleton.js";
+        String jsScript = "script.js";
+
+//        ScriptEngineManager manager = new ScriptEngineManager();
+//        for (ScriptEngineFactory f : manager.getEngineFactories()) {
+//            printBasicInfo(f);
+//            System.out.println();
+//        }
+//
+//
+//        ScriptEngine nashorn = manager.getEngineByName("rhino");
+//        if(nashorn != null) {
+//            System.out.println("rhino is the javascript engine in your JDK.");
+//            try {
+//                //nashorn.eval("print('Hello World!');");
+//                System.out.println("+ Javascript: " + jsScript);
+//                nashorn.eval(new InputStreamReader(V8JavascriptEngine.class.getResourceAsStream(jsScript)));
+//            } catch (ScriptException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        else {
+//            System.out.println("rihno is not present.");
+//        }
+
+        File jsFile = new File("/Users/mukthar.ahmed/Downloads/script.js");
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine jsEngine = factory.getEngineByName("rhino");
+        jsEngine.put("out", System.out);
+
+        try {
+            Reader reader = new FileReader(jsFile);
+            jsEngine.eval(reader);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(jsEngine.toString());
+
+
+
+    }
+
+    public static void printBasicInfo(ScriptEngineFactory factory) {
+        System.out.println("engine name=" + factory.getEngineName());
+        System.out.println("engine version=" + factory.getEngineVersion());
+        System.out.println("language name=" + factory.getLanguageName());
+        System.out.println("extensions=" + factory.getExtensions());
+        System.out.println("language version=" + factory.getLanguageVersion());
+        System.out.println("names=" + factory.getNames());
+        System.out.println("mime types=" + factory.getMimeTypes());
+    }
+
+
+}
