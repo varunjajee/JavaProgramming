483df2791fbb742f168f4300553796121771b86c
Mukthar Ahmed <mukthar.ahmed@inmobi.com>
2016-02-12 15:27:00 +0530

    chained linkedlist

--- src/main/java/com/muks/IntrvAlgorithms/ChainedLinkedList/ChainedLinkedList.java
+++ src/main/java/com/muks/IntrvAlgorithms/ChainedLinkedList/ChainedLinkedList.java
@@ -2,7 +2,21 @@ package com.muks.IntrvAlgorithms.ChainedLinkedList;
 
 /*
  * Created by mukthar.ahmed on 2/10/16.
+
+    1 -> 2 -> 3 -> 4
+    |         |
+    v         V
+    9->10     7->20->22
+    |
+    V
+    11->55
+
+    Output: given me the flatten list
+
+    1->9->11->10->2->3->7->20->22->4
+
  */
+
 public class ChainedLinkedList {
 
     ChainNode head, tail;
@@ -103,57 +117,6 @@ public class ChainedLinkedList {
             this.data = dt;
         }
     }
-
-    /*
-
-1 -> 2 -> 3 -> 4
-|         |
-v         V
-9->10     7->20->22
-|
-V
-11->55
-
-Output: given me the flatten list
-
-1->9->11->10->2->3->7->20->22->4
-
-*/
-
-
-
-//    public void printTree(ChainNode node) {
-//        // base case
-//        if (node.chain == null && node.next == null) {
-//            //System.out.println(node.data);
-//            return;
-//        }
-//
-//        if (node.chain == null && node.next != null) {
-//            System.out.println(node.next.data);
-//            return;
-//        }
-//        else if (node.chain != null && node.next == null) {
-//            System.out.println(node.chain.data);
-//            return;
-//        }
-//
-//        System.out.println(node.data);
-//        printTree(node.chain);
-//        printTree(node.next);
-//
-//
-//
-//        System.out.println(node.data);
-//        if (node.chain != null) {
-//            printTree(node.chain);
-//        }
-//        else {
-//            printTree(node.next);
-//        }
-//
-//
-//    }
 
 } // end class
 
