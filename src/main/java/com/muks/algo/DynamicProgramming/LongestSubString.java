5c42c000960ac7d3cbf9d9d0525110ecdf80cb70
mukthar-am <mukthar.am@gmail.com>
2016-02-26 01:10:29 +0530

    0-1 knapsack problem explaination

--- src/main/java/com/muks/algo/DynamicProgramming/LongestSubString.java
+++ src/main/java/com/muks/algo/DynamicProgramming/LongestSubString.java
@@ -16,130 +16,138 @@ import java.util.HashMap;
 
 public class LongestSubString {
 
-  public static void main(String[] args) {
-    String a = "ABCDAF";
-    String b = "3BCDF";
-    //findLongestSubStr("ababaefabc");
+    public static void main(String[] args) {
+        String a = "ABCDAF";
+        String b = "3BCDF";
+        //findLongestSubStr("ababaefabc");
 
-    //findLongestSubStr("ABDEFGABEF");
-
-    LongestCommonSubString(a, b);
-
-  } // main()
+        //findLongestSubStr("ABDEFGABEF");
 
 
-  /**
-   * Dynamic way of calculating Longest common sub-string
-   */
-  public static void LongestCommonSubString (String x, String y) {
-    int M = x.length();
-    int N = y.length();
+        LongestCommonSubString(a, b);
 
-    int[][] T = new int[M+1][N+1];
-    int max = 0;
+    } // main()
 
-    for (int i = M-1; i >= 0; i--) {
-      for (int j = N-1; j >= 0; j--) {
 
-        if (x.charAt(i) == y.charAt(j)) {
-          T[i][j] = T[i+1][j+1] + 1;
+    /**
+     * Dynamic way of calculating Longest common sub-string
+     */
+    public static void LongestCommonSubString(String x, String y) {
+        int M = x.length();
+        int N = y.length();
 
-          System.out.println(T[i][j]);
+        int[][] T = new int[M + 1][N + 1];
+        int max = 0;
 
-          if (max < T[i][j]) {
-            max = T[i][j];    // calculating the length of longest sub-string
-          }
+        for (int i = M - 1; i >= 0; i--) {
+            for (int j = N - 1; j >= 0; j--) {
+
+                if (x.charAt(i) == y.charAt(j)) {
+                    T[i][j] = T[i + 1][j + 1] + 1;
+
+                    System.out.println(T[i][j]);
+
+                    if (max < T[i][j]) {
+                        max = T[i][j];    // calculating the length of longest sub-string
+                    }
+
+                }
+
+            }
 
         }
 
-      }
+        System.out.println("+ Longest Common Substring is of length = " + max);
+
+        /** Start from the right-most-bottom-most corner and one by one store characters in lcs[] */
+        String res = "";
+        int i = x.length()-1, j = y.length()-1;
+        while (i > 0 && j > 0) {
+            // If current character in X[] and Y are same, then current character is part of LCS
+            if (x.charAt(i) == y.charAt(j)) {
+                res += x.charAt(i);
+                i--;
+                j--;
+            }
+
+            // If not same, then find the larger of two and
+            // go in the direction of larger value
+            else if (T[i - 1][j] > T[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        System.out.println("+ Final String: " + res);
 
     }
 
-    // recover LCS itself and print it to standard output
-    System.out.println("+ Tracing back the matrix for longest common sub-string ....");
-    int i = 0, j = 0;
-    while(i < x.length() && j < y.length()) {
-      if (x.charAt(i) == y.charAt(j)) {
-        System.out.print(x.charAt(i));
-        i++;
-        j++;
-      } else if (T[i+1][j] >= T[i][j+1]) {
-        i++;
-      } else {
-        j++;
-      }
-    }
-    System.out.println();
-
-  }
-
-
-  /**
-   *  A recursive way of finding
-   *  Finding longest substring from a string having unique chars
-   *
-   */
-  public static void findLongestSubStr (String inputString) {
-    System.out.println("+ String: " + inputString);
-
-    HashMap<Character, Integer> map = new HashMap<>();
-    String unique = new String();
-
-    ArrayList<String> subStrings = new ArrayList<>();
-    String longestSubStr = "";
-
-
-    for (char c : inputString.toCharArray()) {
-
-      if ( map.containsKey(c) ) {
-        System.out.println("+ Unique: " + unique + " : 2nd occurrence... +++ \n" );
-
-        subStrings.add(unique);
-        longestSubStr = findMax(longestSubStr, unique); // call the method to declare the longest-substr
-
-        unique = Character.toString(c);
-        map.clear();
-        map.put(c, 1);
-
-      }
-      else {
-        unique += c;
-        map.put(c, 1);
-
-        // In case the longest substring is at end
-        if ( c == inputString.charAt(inputString.length()-1)) {
-          System.out.println("+ UNIQUE: " + unique);
-          subStrings.add(unique);
-          longestSubStr = findMax(longestSubStr, unique);	// call the method to declare the longest-substr
-
-        } // if ()
-
-      } // if-else()
-
-    } // for()
-
-    System.out.println("\n+ Sub Strings: " + subStrings.toString());
-    System.out.println("+ Longest: " + longestSubStr);
-
-  } // findLongestSubStr()
 
 
 
-  public static String findMax (String str1, String str2) {
+    /**
+     * A recursive way of finding
+     * Finding longest substring from a string having unique chars
+     */
+    public static void findLongestSubStr(String inputString) {
+        System.out.println("+ String: " + inputString);
 
-    if ( str1.length() > str2.length() ) {
-      return str1;
+        HashMap<Character, Integer> map = new HashMap<>();
+        String unique = new String();
 
-    } else if (str1.length() == str2.length() ){
-      return str1;
+        ArrayList<String> subStrings = new ArrayList<>();
+        String longestSubStr = "";
 
-    } else {
-      return str2;
 
-    }
+        for (char c : inputString.toCharArray()) {
 
-  } // findMax()
+            if (map.containsKey(c)) {
+                System.out.println("+ Unique: " + unique + " : 2nd occurrence... +++ \n");
+
+                subStrings.add(unique);
+                longestSubStr = findMax(longestSubStr, unique); // call the method to declare the longest-substr
+
+                unique = Character.toString(c);
+                map.clear();
+                map.put(c, 1);
+
+            } else {
+                unique += c;
+                map.put(c, 1);
+
+                // In case the longest substring is at end
+                if (c == inputString.charAt(inputString.length() - 1)) {
+                    System.out.println("+ UNIQUE: " + unique);
+                    subStrings.add(unique);
+                    longestSubStr = findMax(longestSubStr, unique);    // call the method to declare the longest-substr
+
+                } // if ()
+
+            } // if-else()
+
+        } // for()
+
+        System.out.println("\n+ Sub Strings: " + subStrings.toString());
+        System.out.println("+ Longest: " + longestSubStr);
+
+    } // findLongestSubStr()
+
+
+    public static String findMax(String str1, String str2) {
+
+        if (str1.length() > str2.length()) {
+            return str1;
+
+        } else if (str1.length() == str2.length()) {
+            return str1;
+
+        } else {
+            return str2;
+
+        }
+
+    } // findMax()
 
 
 } // class LongestSubString90
