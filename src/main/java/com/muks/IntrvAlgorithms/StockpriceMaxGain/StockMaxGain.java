b321f515299e5ce81e6301c28d4fc61bdcb7f482
mukthar-am <mukthar.am@gmail.com>
2016-03-11 01:30:09 +0530

    stocks buy and sell algorithm

--- src/main/java/com/muks/IntrvAlgorithms/StockpriceMaxGain/StockMaxGain.java
+++ src/main/java/com/muks/IntrvAlgorithms/StockpriceMaxGain/StockMaxGain.java
@@ -5,7 +5,7 @@ package com.muks.IntrvAlgorithms.StockpriceMaxGain;
  */
 public class StockMaxGain {
     public static void main(String[] args) {
-        int[] stocks = {7, 2, 3, 4, 5, 4, 0, 1};
+        int[] stocks = {7, 2, 3, 4, 5, 4, 6, 1};
 
         maxGainStocks(stocks, stocks.length);
     }
@@ -13,15 +13,27 @@ public class StockMaxGain {
     public static void maxGainStocks(int[] prices, int n) {
         int max = prices[n-1];
         int profit = 0;
+        int maxProfit = 0;
+
+        int buy = 0, sell = 0;
+
         for (int j = n-1; j > 0; j--) {
-            if(prices[j]>max) {
+            if(prices[j] > max) {
                 max = prices[j];
+                sell = j;
                 System.out.println("+ Selling day = " + j);
             }
-            profit=profit+max-prices[j];
-            System.out.println("+ Buying day = " + j + ", with profit = " + profit);
+
+            profit = max-prices[j];
+            if (prices[j] < max & maxProfit < profit) {
+                maxProfit = profit;
+                buy = j;
+                System.out.println("+ Buying day = " + j + ", with profit = " + profit);
+            }
 
         }
+
+        System.out.println("Buy on "  + buy + " and sell on " + sell);
 
     }
 
