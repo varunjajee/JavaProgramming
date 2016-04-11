75560870ac3f2f0771fc7760681f9a4cb0031423
mukthar-am <mukthar.am@gmail.com>
2016-04-12 00:37:01 +0530

    2d matrix algos

---
+++ src/main/java/com/muks/Algorithm/AlgosOnArrays/Matrix/HourGlassMaxSum.java
@@ -0,0 +1,60 @@
+package com.muks.Algorithm.AlgosOnArrays.Matrix;
+
+import java.util.Scanner;
+
+/**
+ * Created by mukthar.ahmed on 3/26/16.
+ */
+public class HourGlassMaxSum {
+    public static void main(String[] args) {
+        int[][] matrix1 = {
+            {1, 1, 1, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0},
+            {1, 1, 1, 0, 0, 0},
+            {0, 0, 2, 4, 4, 0},
+            {0, 0, 0, 2, 0, 0},
+            {0, 0, 1, 2, 4, 0}
+        };
+
+        int[][] matrix2 = {
+            {1, 1, 1, 0, 0},
+            {0, 1, 0, 0, 0},
+            {1, 1, 1, 0, 0},
+            {0, 0, 2, 4, 4},
+            {0, 0, 0, 2, 0},
+            {0, 0, 1, 2, 4}
+        };
+
+        maxSumHourglass(matrix1);
+    }
+
+    private static void maxSumHourglass(int[][] arr) {
+
+        int maxSum = 0;
+        int sum = 0;
+
+        int rows = arr.length;
+        int cols = arr[0].length;
+
+        System.out.println("# Row = " + rows + ", Cols = " + cols);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                if ( (i+2 < rows) && (j+2 < cols) ) {
+                    sum = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                        + arr[i + 1][j + 1]
+                        + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+
+                    if (sum > maxSum) {
+                        maxSum = sum;
+                    }
+                }
+            }
+        }
+        System.out.println(maxSum);
+
+    }
+
+
+}
---
+++ src/main/java/com/muks/Algorithm/AlgosOnArrays/Matrix/Traversals.java
@@ -0,0 +1,58 @@
+package com.muks.Algorithm.AlgosOnArrays.Matrix;
+
+/**
+ * Created by mukthar.ahmed on 4/12/16.
+ */
+public class Traversals {
+
+    public static void main(String[] args) {
+        int[][] matrix = AMatrixUtils.getBasicMatrix();
+        spiralOrderPrinting(matrix);
+    }
+
+    static void spiralOrderPrinting(int[][] matrix) {
+        if(matrix.length == 0) {
+            return;
+        }
+
+        /** Initialize our four indexes */
+        int top = 0;
+        int down = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while(true) {
+            /** Print top row   */
+            for(int i = left; i <= right; ++i) {
+                System.out.print( matrix[top][i] + " ");
+            }
+
+            top++;
+            if (top > down || left > right) break;
+
+            /** Print the rightmost column */
+            for (int j = top; j <= down; ++j) {
+                System.out.print( matrix[j][right] + " ");
+            }
+
+            right--;
+            if (top > down || left > right) break;
+
+            /** Print the bottom row */
+            for (int i = right; i >= left; --i) {
+                System.out.print(matrix[down][i] + " ");
+            }
+
+            down--;
+            if (top > down || left > right)  break;
+
+            /** Print the leftmost column */
+            for (int i = down; i >= top; --i) {
+                System.out.print( matrix[i][left] + " ");
+            }
+
+            left++;
+            if (top > down || left > right) break;
+        }
+    }
+}
