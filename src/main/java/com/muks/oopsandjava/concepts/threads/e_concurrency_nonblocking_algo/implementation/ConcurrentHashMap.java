7fe3cd7be0d17881fca4df63f53a6a7a3b42d333
mukthar-am <mukthar.am@gmail.com>
2016-04-15 00:14:22 +0530

    hashmap code base

--- src/main/java/com/muks/oopsandjava/concepts/threads/e_concurrency_nonblocking_algo/implementation/ConcurrentHashMap.java
+++ src/main/java/com/muks/oopsandjava/concepts/threads/e_concurrency_nonblocking_algo/implementation/ConcurrentHashMap.java
@@ -1,13 +1,11 @@
-package com.muks.DSC.HashMap;
+package com.muks.oopsandjava.concepts.threads.e_concurrency_nonblocking_algo.implementation;
 
 /**
  * Created by mukthar.ahmed on 4/14/16.
  * <p>
  * - HashMap implementation, using chaining type of collision resolution
- *
- * Link: http://www.javamadesoeasy.com/2015/02/hashmap-custom-implementation.html
  */
-public class HashMapByChianing {
+public class ConcurrentHashMap {
 
     class HashMap<E> {
         int SIZE_OF_TABLE = 128;
@@ -53,60 +51,57 @@ public class HashMapByChianing {
         }
 
         public E get(E key) {
-            int hash = hashCompute((Integer) key);
+            int index = hashCompute((Integer) key);
 
-            if (table[hash] == null) {
-                return null;
-            }
-            else {
-                if (table[hash].getKey() == key) {
-                    return table[hash].getValue();
-                } else {
-                    HashEntry runner = table[hash].next;
-                    while (runner != null) {
-                        if (runner.getKey() == key) {
-                            return (E) runner.getValue();
-                        } else {
-                            runner = runner.next;
-                        }
+            if (table[index].getKey() == key) {
+                return table[index].getValue();
+            } else {
+                HashEntry runner = table[index].next;
+                while (runner != null) {
+                    if (runner.getKey() == key) {
+                        return (E) runner.getValue();
+                    } else {
+                        runner = runner.next;
                     }
-
-                    return null;
                 }
+
+                return null;
             }
 
         }
+
 
         public E getNonBlocking(E key) {
             int hash = hashCompute((Integer) key);     // throws null pointer exception if key is null
 
             // Try first without locking...
             HashEntry[] tab = table;
-            //int index = hash & (tab.length - 1);
-            HashEntry first = tab[hash];
+            int index = hash & (tab.length - 1);
+            HashEntry first = tab[index];
             HashEntry e;
 
             for (e = first; e != null; e = e.next) {
                 if (e.hash == hash && (key == e.getKey())) {
                     Object value = e.value;
                     // null values means that the element has been removed
-                    if (value != null)
+                    if (value != null) {
                         return (E) value;
-                    else
+                    } else {
                         break;
+                    }
                 }
             }
 
             // Recheck under synch if key apparently not there or interference
-
-            synchronized(this) {
+            synchronized (this) {
                 tab = table;
                 hash = hash & (tab.length - 1);
                 HashEntry newFirst = tab[hash];
                 if (e != null || first != newFirst) {
                     for (e = newFirst; e != null; e = e.next) {
-                        if (e.hash == hash && (key == e.getKey()))
+                        if (e.hash == hash && (key == e.getKey())) {
                             return (E) e.value;
+                        }
                     }
                 }
                 return null;
@@ -114,40 +109,11 @@ public class HashMapByChianing {
         }
 
 
-        public boolean remove(E key) {
-            int hash = hashCompute((Integer) key);
-
-            if (table[hash] == null) {
-                return false;
-            }
-            else {
-                HashEntry<E> previous = null;
-                HashEntry<E> current = table[hash];
-
-                while (current != null) {
-                    if (current.getKey() == key) {
-                        if (previous == null) {     /** Hash key is the head of the linkedlist */
-                            table[hash] = table[hash].next;
-                            return true;
-                        }
-                        else {      /** key found and is in mid or even tail case is handled */
-                            previous.next = current.next;
-                            return true;
-                        }
-                    }
-                    else {
-                        previous = current;
-                        current = current.next;
-                    }
-                }
-            }
-
-        }
-        public int hashCompute(int h) {
-//            h ^= (h >>> 20) ^ (h >>> 12);
-//            int hashCode = h ^ (h >>> 7) ^ (h >>> 4);
+        public int hashCompute(int key) {
+//            key ^= (key >>> 20) ^ (key >>> 12);
+//            int hashCode = key ^ (key >>> 7) ^ (key >>> 4);
             //return hashCode % SIZE_OF_TABLE;
-            return h % SIZE_OF_TABLE;
+            return key % SIZE_OF_TABLE;
         }
     }
 
@@ -175,14 +141,15 @@ public class HashMapByChianing {
 
 
     public static void main(String[] args) {
-        HashMapByChianing.HashMap<Integer> hashMap = new HashMapByChianing().new HashMap();
+        HashMap<Integer> hashMap = new ConcurrentHashMap().new HashMap();
 
-        hashMap.put(10, 20);
-        hashMap.put(20, 11);
-        hashMap.put(21, 1);
+//        hashMap.put(10, 20);
+//        hashMap.put(20, 11);
+//        hashMap.put(21, 1);
         hashMap.put(20, 10);
 
-        int value = hashMap.get(20);
+        //int value = hashMap.get(20);
+        int value = hashMap.getNonBlocking(20);
         System.out.println("# Val = " + value);
 
     }
