5e6784be40424615f276c294c8bc1d8f5e843c92
mukthar-am <mukthar.am@gmail.com>
2016-05-09 22:15:44 +0530

    commit 11.1

--- src/main/java/com/muks/Algorithm/AlgosOnArrays/Basics/Segregate0sAnd1s.java
+++ src/main/java/com/muks/Algorithm/AlgosOnArrays/Basics/Segregate0sAnd1s.java
@@ -22,7 +22,7 @@ public class Segregate0sAnd1s {
 	// in place segration of 0s and 1s: 
 	// segregate using modification of name
 	
-	public static void sortZeroOne(int[] a) {
+	private static void sortZeroOne(int[] a) {
 		if (a == null || a.length==1) {
 			return;
 		}
@@ -45,44 +45,7 @@ public class Segregate0sAnd1s {
 				j--;
 			}
 		}
-
-		return;
-
 	}
 
-
-	// ############################################################################################################
-	// in place segration of 0s and 1s: 
-	// segregate using swap
-	public static void segregate0and1 (int[] arr) {
-		int i = 0; 
-		int j = arr.length-1;
-
-		while (i < j) {
-			while (arr[i] == 0 && i < j) {
-				i++;
-			}
-
-			while (arr[j] == 1 && i < j) {
-				j--;
-			}
-
-			if (i < j) {
-				swap(arr, i, j);
-			}
-
-		}
-
-	} // segregate0and1()
-
-	
-	// #################################################################################
-	// swap elements of array
-	public static void swap (int[] a, int i, int j) {
-		a[i] = a[i] ^ a[j];
-		a[j] = a[j] ^ a[i];
-		a[i] = a[i] ^ a[j];
-	}
-	
 	
 }	// end class()
--- src/main/java/com/muks/Algorithm/AlgosOnArrays/Basics/SortPartiallySortedArray.java
+++ src/main/java/com/muks/Algorithm/AlgosOnArrays/Basics/SortPartiallySortedArray.java
@@ -2,50 +2,51 @@ package com.muks.Algorithm.AlgosOnArrays.Basics;
 
 public class SortPartiallySortedArray {
 
-	/**
-	 * In place sorting of a partially sorted array
-	 */
-	public static void main(String[] args) {
-		//int[] arr = {1, 4, 5, 7, 8, 9, 2, 3, 6, 10, 11};
-		int[] arr = {7, 8, 9, 10, 11, 1, 2, 3, 4, 5, 6, };
-		sort(arr);
-		
-		String s = "";
-		for (int i : arr) {
-			s += i + " ";
-		}
-		System.out.println(s);
-	}
-	
-	
-	public static void sort(int[] a) {
-		int left = 0;
-		int right = a.length - 1;
+    /**
+     * In place sorting of a partially sorted array
+     */
+    public static void main(String[] args) {
+        //int[] arr = {1, 4, 5, 7, 8, 9, 2, 3, 6, 10, 11};
+        int[] arr = {7, 8, 9, 10, 11, 1, 2, 3, 4, 5, 6,};
+        sort(arr);
 
-		// find beginning of second sub-array
-		while(right != left && a[right] > a[right-1]) {
-			System.out.println("right: " + right + " left: " + left + " : " + a[right] + " - " + a[right-1]);
-			right--;
-		}
+        String s = "";
+        for (int i : arr) {
+            s += i + " ";
+        }
+        System.out.println(s);
+    }
 
-		while(right != a.length && left != a.length) {
-			if (a[left] < a[right]) {
-				left++;
-			} else {
-				System.out.println(a[left] + " < " + a[right]);
-				swap(a, left, right);
-				right++;
-			}
-		}
-		return;
 
-	}
+    public static void sort(int[] a) {
+        int i = 0;
+        int j = a.length - 1;
 
-	public static void swap(int[] a, int l, int r) {
-		int tmp = a[l];
-		a[l] = a[r];
-		a[r] = tmp;
-	}
+        // find beginning of second sub-array
+        while ( i < j ) {
+            if (a[j] < a[j-1]) {
+                break;
+            }
+            j--;
+        }
+
+
+        while (j != a.length && i != a.length) {
+            if (a[i] < a[j]) {
+                i++;
+            } else {
+                System.out.println(a[i] + " < " + a[j]);
+                swap(a, i, j);
+                j++;
+            }
+        }
+    }
+
+    public static void swap(int[] a, int l, int r) {
+        int tmp = a[l];
+        a[l] = a[r];
+        a[r] = tmp;
+    }
 
 
 }
