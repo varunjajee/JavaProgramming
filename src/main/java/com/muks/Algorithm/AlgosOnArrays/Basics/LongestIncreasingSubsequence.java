e95550c409feae18a26f1ee9f9823895d5cbb0d2
mukthar-am <mukthar.am@gmail.com>
2016-05-11 23:16:05 +0530

    increasing sub sequence and bitonic sub sequence

--- src/main/java/com/muks/Algorithm/AlgosOnArrays/Basics/LongestIncreasingSubsequence.java
+++ src/main/java/com/muks/Algorithm/AlgosOnArrays/Basics/LongestIncreasingSubsequence.java
@@ -5,30 +5,27 @@ import java.util.List;
 
 /**
  * Created by mukthar.ahmed on 5/6/16.
- *
- *  - Longest increasing sub-sequence from an array.
- *
  */
 public class LongestIncreasingSubsequence {
-    public static void main(String[] args) {
-        int[] arr1 = {1, 2, 5, 9, 16};
-        int[] arr2 = {3, 4, -1, 5, 8, 2, 3, 12, 7, 9, 10};
 
-        increasingSubSeq(arr2);
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 5, 9, 16};
+        //int[] arr = {3, 4, -1, 5, 8, 2, 3, 12, 7, 9, 10};
+
+        increasingSubSeqLength(arr);
+        increasingSubSeqElementsAndLength(arr);
     }
 
-    /** ================================================================================
-     *  O(n2) solution by simply tracking the consecutive largest element
-     */
 
-    public static void increasingSubSeq(int[] arr) {
+    private static void increasingSubSeqLength(int[] arr) {
         List<Integer> myList = new ArrayList<>();
         int len = arr.length;
 
-        /** calculate the increasing sub sequence */
         int[] table = new int[arr.length];
         table[0] = 1;
 
+        /** Calculate increasing sub sequence for the 1st half */
         for (int i = 1; i < len; i++) {
             table[i] = (arr[i] > arr[i - 1]) ? table[i - 1] + 1 : 1;
         }
@@ -45,12 +42,45 @@ public class LongestIncreasingSubsequence {
         System.out.println("# max = " + max);
     }
 
-    private static void printArray(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (int i : arr) {
-            sb.append(" " + i);
+
+    /**
+     * ================================================================================
+     * O(n2) solution by simply tracking the consecutive largest element
+     */
+    private static void increasingSubSeqElementsAndLength(int[] arr) {
+        int[] table;
+        int max = 0;
+
+        table = new int[arr.length];
+
+        List<Integer> myList = new ArrayList<>();
+        myList.add(arr[0]);
+
+        /** Calculate increasing sub sequence for the 1st half */
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                table[i] = table[i - 1] + 1;
+                if (!myList.contains(arr[i])) {
+                    myList.add(arr[i]);
+                }
+            } else {
+                table[i] = 1;
+                myList.remove(arr[i]); // remove if not a right candidate
+            }
         }
-        System.out.println(sb.toString() + "\n");
+
+
+        /** Pick maximum of all LIS values */
+        for (int i = 0; i < arr.length; i++) {
+            if (max < table[i]) {
+                max = table[i];
+            }
+        }
+
+        System.out.println("# Size of max increasing sub-sequence = " + max);
+        System.out.println(myList.toString());
     }
+
+
 }
 
